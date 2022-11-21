4f9ce02fa0f0c8ebf02f620c7878004b26439a8c
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-21 15:47:39 +0100

    `Remove` darkMode: false from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
   theme: {
     extend: {
       colors: {
